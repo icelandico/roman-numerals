6bcd81e8544f4d3d2297bf9c3f165c4ae5fc1789
Michal Muszynski <m.muszy@gmail.com>
2018-11-24 21:06:09 +0100

    Adding style for gatsby app

---
+++ src/components/Header/header.js
@@ -0,0 +1,8 @@
+import styled from 'styled-components'
+
+const StyledHeader = styled.h1`
+  font-family: 'Helvetica';
+  font-size: 2rem;
+`
+
+export default StyledHeader
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,45 @@
 import React from 'react'
+import styled from 'styled-components'
 import { Link } from 'gatsby'
+import Header from '../components/Header/header'
+
+const StyledBanner = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+  margin: 0;
+  padding: 0;
+  width: 100%;
+  height: 100vh;
+  background-color: #201858;
+  color: #847EB1;
+  text-decoration: none;
+  `;
+
+const StyledLink = styled(Link)`
+  text-decoration: none;
+  color: #fff;
+`;
+
+const HeaderWrapper = styled.div`
+  display: flex;
+  align-items: center;
+
+`
+
 
 const IndexPage = () => (
-  <div>
-    <h1>This will be the Roman Numeral Converter App</h1>
-    <Link to="/page-2/">Go to page 2</Link>
-  </div>
+  <>
+    <StyledBanner>
+      <HeaderWrapper>
+        <Header>Roman Numerals Converter</Header>
+      </HeaderWrapper>
+      <StyledLink to="/converter/">Go to converter</StyledLink>
+      <StyledLink to="/about/">Go to about page</StyledLink>
+    </StyledBanner>
+  </>
+
 )
 
 export default IndexPage
