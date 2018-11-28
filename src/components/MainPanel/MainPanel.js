0bb5cd30165420d914738e99da03f94d4191f029
Michal Muszynski <m.muszy@gmail.com>
2018-11-28 22:44:27 +0100

    Added algorithm for converting numbers

--- src/components/MainPanel/MainPanel.js
+++ src/components/MainPanel/MainPanel.js
@@ -24,7 +24,6 @@ const InputPanel = styled.div`
 `
 
 class Input extends Component {
-
   state = {
     input: '',
     result: ''
@@ -37,9 +36,22 @@ class Input extends Component {
   }
 
   handleConvert = () => {
+    const inputNumber = parseInt(this.state.input)
     this.setState({
-      result: this.state.input
+      result: this.convertNumber(inputNumber)
     })
+  }
+
+  convertNumber = (number) => {
+    const latinNums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 20, 30, 40, 50, 60, 70, 80, 90, 100, 200, 300, 400, 500, 600, 700, 800, 900, 1000, 2000, 3000, 4000]
+    const romanNums = ['I', 'II', 'III', 'IV', 'V','VI', 'VII', 'VIII', 'IX', 'X', 'XX', 'XXX', 'XL', 'L', 'LX', 'LXX', 'LXXX', 'XC', 'C', 'CC', 'CCC', 'CD', 'D', 'DC', 'DCC', 'DCCC', 'CM', 'M', 'MM', 'MMM', 'MMMM']
+    const thousands = Math.floor(number / 1000) * 1000;
+    const hundreds = (Math.floor((number % 1000) / 100)) * 100;
+    const decimals = (Math.floor((number % 100) / 10)) * 10;
+    const nums = number % 10;
+    const destructurizedNumber = [thousands, hundreds, decimals, nums];
+    const romanNumsArray = destructurizedNumber.map(number => romanNums[latinNums.indexOf(number)])
+    return romanNumsArray.join('')
   }
 
   render() {
@@ -61,7 +73,6 @@ class Input extends Component {
       </InputPanel>
     )
   }
-
 }
 
 export default Input
