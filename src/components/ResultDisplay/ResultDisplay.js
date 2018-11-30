bb015ab31faeb5490964bb04f681b5cc4374f799
Michal Muszynski <m.muszy@gmail.com>
2018-11-30 13:29:51 +0100

    Show result in proper way

--- src/components/ResultDisplay/ResultDisplay.js
+++ src/components/ResultDisplay/ResultDisplay.js
@@ -1,10 +1,16 @@
 import React from 'react'
 import styled from 'styled-components'
 
+const ResultTab = styled.div`
+  font-size: 4vh;
+  letter-spacing: 0.5rem;
+  text-indent: 0.5rem;
+`
+
 const ResultDisplay = (props) => (
-  <>
+  <ResultTab>
     <p>{props.result ? props.result : '' }</p>
-  </>
+  </ResultTab>
 )
 
 export default ResultDisplay
